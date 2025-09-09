b229fdb9faf35326497db019e77076a0efe942a3
F1RM3X <firmax59@gmail.com>
2025-09-09 16:37:44 -0400

    milieu_etape3

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,22 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import supertest from 'supertest';
+import app from '../../src/app';
+
+const request = supertest(app);
+const testNom1 = 'Jean-Marc';
+const testNom2 = 'Pierre';
+
+beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
+});
 
 describe('redemarrerJeu.test.ts', () => {
   it("devrait implémenter test", async () => {
     throw new Error("Ce test n'a pas été défini")
   });
 });
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+    it();

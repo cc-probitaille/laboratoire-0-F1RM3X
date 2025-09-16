d52969bcb6aeb6bb0ddd4fe14108182c60b78d05
F1RM3X <firmax59@gmail.com>
2025-09-16 10:51:15 -0400

    fin etape 3

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -9,14 +9,24 @@ const testNom1 = 'Jean-Marc';
 const testNom2 = 'Pierre';
 
 beforeAll(async () => {
-    await request.post('/api/v1/jeu/demarrerJeu').send({ nom: testNom1 });
-});
-
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
-  });
+    await request.post('/api/v1/jeu/redemarrerJeu').send({ nom: testNom1 });
+    await request.post('/api/v1/jeu/creerJoueur').send({ nom: testNom2 });
 });
 
 describe('GET /api/v1/jeu/redemarrerJeu', () => {
-    it();
+  it(`devrait redémarrer le jeu avec succès ${testNom1}`, async () => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu');
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+
+  it(`devrait ne plus rester de joueurs après redémarrage`, async () => {
+        const response = await request.get('/api/v1/jeu/jouer/' +testNom1);
+        const response2 = await request.get('/api/v1/jeu/jouer/' +testNom2);
+        expect(response.status).toBe(200);
+        expect(response2.status).toBe(200);
+        expect(response.body.length).toBe(0);
+        expect(response2.body.length).toBe(0);
+    });
+
+});
